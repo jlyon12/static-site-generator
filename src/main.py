f17b55b80d1db0ddc7bf25be3cbe72fd2f55111e
Jarrett Lyon <64499632+jlyon12@users.noreply.github.com>
2025-03-02 16:09:31 -0700

    Create textnode class

---
+++ src/main.py
@@ -0,0 +1,11 @@
+from textnode import TextNode, TextType
+
+
+def main():
+    test = TextNode(
+        "This is some anchor text", TextType.LINK_TEXT, "https://www.boot.dev"
+    )
+    print(test)
+
+
+main()
