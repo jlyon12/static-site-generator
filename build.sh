bd8f11540ec879d68277f086b4f0894e8c514eb6
Jarrett Lyon <64499632+jlyon12@users.noreply.github.com>
2025-03-07 20:31:17 -0700

    fix: repo variable in build script

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 SCRIPT_PATH=${1:-src/main.py}
-REPO=https://github.com/jlyon12/static-site-generator/
+REPO=/static-site-generator/
 
 if ! command -v python3 &>/dev/null; then
     echo "Error: python3 is not installed or not in PATH." >&2
